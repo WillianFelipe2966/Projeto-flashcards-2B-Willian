c6d9f3bf09904410808c89689acaf84a638dc31b
WillianFelipe2966 <silva.willian27@escola.pr.gov.br>
2025-08-13 15:56:40 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,7 @@
+criaCartao('Sobrevivente', 'Quem é Meg Thomas?', 'Uma corredora habilidosa e sobrevivente inicial.');
+criaCartao('Assassino', 'Qual é a habilidade do Caçador?', 'Arremessar machados com precisão mortal.');
+criaCartao('Perks', 'O que faz "Sprint Burst"?', 'Permite correr 150% mais rápido por 3 segundos.');
+criaCartao('Mapas', 'Qual mapa tem o Hospital Abandonado?', 'O Lery’s Memorial Institute.');
+criaCartao('Itens', 'Para que serve a lanterna?', 'Cegar o assassino e ajudar companheiros.');
+criaCartao('Mecânica', 'Como funciona o gancho?', 'Sobreviventes são pendurados nele até serem sacrificados.');
+criaCartao('Lore', 'Quem é o Entidade?', 'Uma força sombria que comanda os testes de sobrevivência.');
